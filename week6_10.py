fbb9397ef5cf200a708d64b6be49109a1198ff7e
Marco-de-Sa <marcojb.desa@gmail.com>
2024-10-22 11:33:52 +0100

    added python exercise code to repo

---
+++ week6_10.py
@@ -0,0 +1,3 @@
+departments = ["finance", "software", "management","maintenance"]
+dep = input("choose a department from the list:\n- finance\n- software\n- management\n- maintenance\nanswer: ")
+years = int(input("how many years have you worked for your department"))
