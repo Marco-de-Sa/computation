da886a32d58f8f87b86a8bd031a015f39dd27a5a
Marco-de-Sa <marcojb.desa@gmail.com>
2024-10-22 12:30:07 +0100

    added python exercise code to repo

--- week7_Theory.py
+++ week7_Theory.py
@@ -1,9 +1,12 @@
 name, SD, classNum, classRef = input("input your name: "), input("input your student number: "), int(input("how many classes do you have?: ")), []
 scholarships = ["20221111", "20222222", "20223333", "20224444", "20219999", "20218888", "20214444"]
+
 for i in range(classNum):
     classRef.append(input("class reference: "))
+
 price = []
 costFinal=0
+
 for i in range(classNum):
     if classRef[i][-3] == "I" and classRef[i][-2] == "I":
         price.append(150)
@@ -14,7 +17,9 @@ for i in range(classNum):
 if SD in scholarships:
     for i in range(len(price)):
         price[i] = float(price[i])*(1-0.15)
+
 print("------------------------------------------------------")
+
 print(f"Name: {name}\nstudent number: {SD}\nNumber of classes: {classNum}\nList of classes:")
 for i in range(classNum):
     print(f"- {classRef[i]}")
