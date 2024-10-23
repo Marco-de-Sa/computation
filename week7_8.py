8b6b2f9c3a558676e7439db7847a8871fdc951dc
Marco-de-Sa <marcojb.desa@gmail.com>
2024-10-23 19:41:52 +0100

    added python exercise code to repo

--- week7_8.py
+++ week7_8.py
@@ -2,6 +2,5 @@ numChild, childList, letters = int(input("how many children would you like to ha
 for i in range(numChild):
     childList.append(input(f"name of child number {i+1}: "))
 for i in range(len(childList)):
-    temp = childList[i]
-    print(len(temp))
-    letters += len(temp)
+    print(len(childList[i]))
+    letters += len(childList[i])
