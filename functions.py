5df2b7b674cf57cdad75eb310ccbe8dad323af75
Marco-de-Sa <marcojb.desa@gmail.com>
2024-10-25 12:14:19 +0100

    added python exercise code to repo

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 def add(num1, num2):
     temp = num1 + num2
     print(temp)
+    if temp <= 10:
+        add(temp, temp)
     return "numbers added successfully :)"
-print(add(1,2))
+print(add(1,1))
