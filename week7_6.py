232d372fbb22d10aa48169fd5b3c37fd643853d5
Marco-de-Sa <marcojb.desa@gmail.com>
2024-10-25 11:58:03 +0100

    added python exercise code to repo

--- week7_6.py
+++ week7_6.py
@@ -2,4 +2,6 @@ gameCount, totalCost = int(input("how many games have you bought this month: "))
 for i in range(gameCount):
     totalCost += float(input(f"how much did game {i+1} cost?: "))
 if totalCost > 100:
-    print("you have issues")
+    print(f"you have issues, also the total cost is {totalCost}")
+else:
+    print(f"the total cost is {totalCost}")
