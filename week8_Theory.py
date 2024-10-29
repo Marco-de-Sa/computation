261c0a97dfc7aea851fe9f55b2810d726fdc5658
Marco-de-Sá <marcojb.desa@gmail.com>
2024-10-29 23:31:10 +0000

    commiting code from the first three weeks

--- week8_Theory.py
+++ week8_Theory.py
@@ -4,8 +4,8 @@
 #         print("#",end=" ")
 #     print()
 
-# for i in range(10):
-#     for y in range(10):
-#         print(f"{i+1} x {y+1} = {(i + 1) * (y + 1)}", end="\t\t")
-#     print()
+for i in range(10):
+    for y in range(10):
+        print(f"{i+1} x {y+1} = {(i + 1) * (y + 1)}", end="\t\t")
+    print()
 
