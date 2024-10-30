00d13c66707473e5cee0fb49ef5dc08f072f6e69
Marco-de-Sa <marcojb.desa@gmail.com>
2024-10-30 08:51:57 +0000

    added python exercise code to repo

--- week8_4.py
+++ week8_4.py
@@ -1,5 +1,5 @@
 ltNum, howManyNum, i = [], int(input("how many numbers do you want to input: ")), 0
-while i <howManyNum:
+while i < howManyNum:
     ltNum.append(int(input("input an integer: ")))
     i+=1
 for i in range(len(ltNum)):
