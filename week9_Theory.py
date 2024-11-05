f6ac3f4c865c07b25909fc7ebd7181fde120007c
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-05 12:02:16 +0000

    commiting code for computation

---
+++ week9_Theory.py
@@ -0,0 +1,12 @@
+def greet(hello, world):
+    print(f"{hello} {world}")
+    x = range(10)
+    print(x)
+    return 5**2
+
+print(greet("hey", "john"))
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("sorry you cannot divide by zero")
+# print(5/0)
