7f620dac3140650179aaa0427e39726db8f4937d
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-05 12:23:02 +0000

    commiting code for computation

--- week9_Theory.py
+++ week9_Theory.py
@@ -1,16 +1,19 @@
-def greet(hello, world):
-    print(f"{hello} {world}")
-    x = range(10)
-    print(x)
-    return 5**2
+try:
+    def greet(hello, world):
+        print(f"{hello} {world}")
+        x = range(10)[0]
+        print(x)
+        return 5**2
 
-print(greet("hey", "john"))
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("sorry you cannot divide by zero")
-# print(5/0)
-try:
-    int(input("don't input an integer :)"))
-except ValueError:
-    print("good job")
+    print(greet("hey", "john"))
+    try:
+        print(5/0)
+    except ZeroDivisionError:
+        print("sorry you cannot divide by zero")
+    # print(5/0)
+    try:
+        int(input("don't input an integer :)\n"))
+    except ValueError:
+        print("good job")
+except KeyboardInterrupt:
+    print("goodbye")
