ff58a47663b6d236ee6ec19f4efdbab69be4bf00
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-08 11:36:07 +0000

    commiting code for computation

--- functionTheoryLecture.py
+++ functionTheoryLecture.py
@@ -13,9 +13,11 @@
 # repeat_text("hey guys!", 5)
 
 # def plus_4(n):
+#     """adds 4 to the parsed integer"""
 #     n+=4
 #     return n
 # print(plus_4(4))
+# print(plus_4(4).__doc__)
 
 # complex data types that are parsed into the function don't need to be returned because the value on the
 # parameters and the value of the parsed complex datatype are addressed to the same pointer
@@ -27,3 +29,5 @@
 # plus_4_arr(vet)
 # print(vet)
 # this'll print out 14 and 19
+
+"""5/0"""
