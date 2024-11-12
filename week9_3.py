412106d6827be9c67756aa28002b94b7bc3ff9c4
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-12 14:05:49 +0000

    commiting code for computation

--- week9_3.py
+++ week9_3.py
@@ -1,6 +1,6 @@
 name, count = [], 0
 for i in range(5):
-    name.append(input("input a name:"))
+    name.append(input("input a name: "))
 for i in range(len(name)):
     temp = name[i]
     if temp[0].lower() == "a":
