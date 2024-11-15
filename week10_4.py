d7b760b98a6b35540c6fb554077ce3fca9b1adf6
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-15 12:43:53 +0000

    commiting code for computation

--- week10_4.py
+++ week10_4.py
@@ -1,6 +1,7 @@
 name, nameNew = ["Ann", "Mary", "Jane", "Sue"], []
-for i in range(len(name)):
-    for j in range(len(name)):
-        if name[i]!=name[j]:
-            nameNew.append(f"{name[i]} {name[j]}")
+nameNew = [f"{name[i]} {name[j]}" for i in range(len(name)) for j in range(len(name)) if name[i]!=name[j]]
+# for i in range(len(name)):
+#     for j in range(len(name)):
+#         if name[i]!=name[j]:
+#             nameNew.append(f"{name[i]} {name[j]}")
 print(nameNew)
