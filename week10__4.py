294f68a446b39dac3f78a5d34a2ffbb42cd2291a
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-18 10:21:26 +0000

    commiting code for computation

--- week10__4.py
+++ week10__4.py
@@ -1,3 +1,4 @@
 x, y = ["A", "B", "C", "D", "E", "F", "G", "H"], [1, 2, 3, 4, 5, 6, 7, 8]
 chess = [f"{x[i]}{y[j]}" for i in range(8) for j in range(8) if (i%2 == 0 and j%2 ==0) or (i%2 != 0 and j%2 != 0)]
 print(chess)
+#test
