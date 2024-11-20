6c409f1091f509fc17ea8f52df49ed5779459805
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-20 09:11:28 +0000

    commiting code for computation

--- week10__1.py
+++ week10__1.py
@@ -2,9 +2,10 @@ from random import randint
 
 card = [int(input("input a number between 1 and 90: ")) for i in range(3)]
 bingo = [randint(1, 90) for i in range(70)]
+print(bingo)
 hitCount = 0
 hits = [bingo[i] for i in range(len(bingo)) if bingo[i] in card]
 if len(hits) >= 3:
-    print("Threengo!")
+    print(f"Threengo!{hits}")
 else:
     print(f"better luck next time, the numbers you got right were {hits}")
--- week10__2.py
+++ week10__2.py
@@ -1,9 +1,7 @@
-def match_list(L1, L2):
-    templist = []
-    for i in range(len(L1)):
-        templist.append(f"{L1[i]} {L2[i]}")
-    return templist
 groupNum = int(input("how many people are in the group: "))
-groupFirstName = [input(f"first name of group member {i}: ") for i in range(groupNum)]
-groupJobName = [input(f"job name of group member {i}: ") for i in range(groupNum)]
-print(match_list(groupFirstName, groupJobName))
+groupFirstName = [input(f"first name of group member {i+1}: ") for i in range(groupNum)]
+groupJobName = [input(f"job name of group member {i+1}: ") for i in range(groupNum)]
+templist = []
+for i in range(len(groupFirstName)):
+    templist.append(f"{groupFirstName[i]}, {groupJobName[i]}")
+print(templist)
