2601d96223dc766915f93d6cb493453341e5b00a
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-27 09:17:54 +0000

    commiting code for computation

--- week12_2.py
+++ week12_2.py
@@ -1,21 +1,20 @@
 import math
-
-def optionOne():
+def option_one():
     st = input("input a word")
     print(st.upper())
-def optionTwo():
+def option_two():
     colour = input("what is your favourite colour?")
     print(f"{colour} is a terrible choice")
-def optionThree():
+def option_three():
     print(math.pi)
 while True:
     x = int(input("choose 1, 2 or 3"))
     if x == 1:
-        optionOne()
+        option_one()
         break
     if x == 2:
-        optionTwo()
+        option_two()
         break
     if x == 3:
-        optionThree()
+        option_three()
         break
