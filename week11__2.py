ed7d871ec18c8222350c27522745297ce8bf4944
Marco-de-Sá <marcojb.desa@gmail.com>
2024-11-27 09:06:55 +0000

    commiting code for computation

--- week11__2.py
+++ week11__2.py
@@ -2,4 +2,3 @@ rows = int(input("how many rows: "))
 columns = int(input("how many columns: "))
 Matrix = [[int(input("input a num: ")) for i in range(columns)] for j in range(rows)]
 print(Matrix)
-
