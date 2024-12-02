ed287040c80b6d5f2f7a13d1c59016c132842cd5
Marco-de-Sá <marcojb.desa@gmail.com>
2024-12-02 12:49:05 +0000

    commiting code for computation

--- week12__2.py
+++ week12__2.py
@@ -6,7 +6,7 @@ def var_in_list(arr, var):
             positions.append(i)
     print(f"{var} appears in indexes {positions}")
 
-# defines a premade list and variable and parses it into the var_in_list function
+# defines a pre-made list and variable and parses it into the var_in_list function
 l = [1, 2, 3, 2]
 v = 2
 var_in_list(l, v)
