dcf2704ad6a49614f63754ade3e8e70c82807eeb
Marco-de-Sá <marcojb.desa@gmail.com>
2024-12-04 09:02:11 +0000

    commiting code for computation

--- week12__2.py
+++ week12__2.py
@@ -6,7 +6,7 @@ def var_in_list(arr, var):
             positions.append(i)
     print(f"{var} appears in indexes {positions}")
 
-# defines a pre-made list and variable and parses it into the var_in_list function
+# defines a user-made list of a user specified length and parses it into the var_in_list function
 num = int(input("input a the length of a list: "))
 l = [int(input("input a number: ")) for i in range(num)]
 v = int(input("input the number you'd like to search for: "))
