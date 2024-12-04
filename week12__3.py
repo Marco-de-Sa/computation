4bec16e13e81ae81390a367a151d973e54e57aee
Marco-de-Sá <marcojb.desa@gmail.com>
2024-12-04 09:07:45 +0000

    commiting code for computation

--- week12__3.py
+++ week12__3.py
@@ -1,5 +1,6 @@
 # takes two ints and puts a to the power of b afterward it returns the result
 def to_power(a, b):
     return a**b
+
 # asks for two numbers, parses them into to_power and then prints out the returned value
 print(to_power(int(input("input first num: ")), int(input("input second num: "))))
