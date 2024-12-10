b009e29b01560ea506d7fdf257aa33e2e8733e79
Marco-de-Sá <marcojb.desa@gmail.com>
2024-12-10 11:58:16 +0000

    commiting code for computation

--- week13__4.py
+++ week13__4.py
@@ -1,11 +1,12 @@
 def count_prime(num):
     listOfPrimes = []
     for i in range(num+1):
-        for j in range(2, num // 2 + 1):
-            if i % j == 0:
+        for j in range(2, i // 2 + 1):
+            if i % j != 0 and i % (j+1) != 0:
                 listOfPrimes.append(i)
                 break
-
+            elif i%j == 0:
+                break
     return listOfPrimes
 
 print(count_prime(int(input("gimme a number and I'll check if it a prime number: "))))
