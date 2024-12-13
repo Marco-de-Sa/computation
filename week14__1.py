5825548b1fa565134675cffe1d4455b11288e5ea
Marco-de-Sá <marcojb.desa@gmail.com>
2024-12-13 12:29:49 +0000

    commiting code for computation

--- week14__1.py
+++ week14__1.py
@@ -3,6 +3,6 @@ M = [[99,88,77],[66,55,44],[33,22,11]]
 for i in range(len(M)):
     som_van = 0
     for j in range(len(M[i])):
-        som_van+=j+1
+        som_van+=j
         M[i][j] = som_van
 print(M)
