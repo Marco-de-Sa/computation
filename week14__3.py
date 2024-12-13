75e5a010c965acf376e8d81ccd2ca0f9ecc2fe7c
Marco-de-Sá <marcojb.desa@gmail.com>
2024-12-13 12:35:09 +0000

    commiting code for computation

--- week14__3.py
+++ week14__3.py
@@ -4,6 +4,7 @@ def get_list():
         list.append(int(input("give me a number: ")))
     return list
 
+
 def get_min(l):
     temp = l[0]
     for i in l:
@@ -11,10 +12,12 @@ def get_min(l):
             temp = i
     return temp
 
+
 def can_divide(a, b):
     if a%b == 0:
         return True
     else:
         return False
+
 
 print(can_divide(get_min(get_list()), 10))
